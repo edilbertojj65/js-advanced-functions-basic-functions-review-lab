41ff0816a9dee422f2004e1e38a2b3562d89ec59
edilbertojj65 <edilbertojj65@gmail.com>
2021-04-01 15:32:10 -0400

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code here
+
+function saturdayFun(string="roller-skate") {
+  return `This Saturday, I want to ${string}!`;
+ }
+
+saturdayFun("roller-skate");
+saturdayFun("bathe my dog");
+
+let mondayWork = function(string="go to the office"){
+
+      return `This Monday, I will ${string}.`;
+
+}
+
+ mondayWork("work from home.");
+ mondayWork();
+
+ let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
+
+  const Calculator = {
+    add: function(a,b) {
+      return a + b;
+    },
+    subtract: function(a,b) {
+      return a - b
+    },
+    multiply: function(a,b) {
+      return a * b
+    },
+    divide: function(a,b) {
+      return a / b;
+    }
+  }
+
+  let actionApplyer = function(start, ray) {
+    let a = start
+  
+    for (let i = 0; i < ray.length; i++ ){
+      a = ray[i](a)
+    }
+  
+    return a
+  }  
